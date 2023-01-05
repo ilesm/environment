916e5442e5bcffe4ae3f641b2fdb40b735596fcc
Michael Iles <michael.iles@gmail.com>
2023-01-05 14:10:58 -0500

    Simplify the binding to not use `V` visual mode. Add a separate visual mode binding for multiple lines.

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -2,6 +2,7 @@
 
 require('mike')
 
-vim.api.nvim_set_keymap("n", "<Space>", "V:luado return mikemike(line)<Enter>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Space>", ":.luado return mikemike(line)<Enter>", { noremap = true })
+vim.api.nvim_set_keymap("v", "<Space>", ":luado return mikemike(line)<Enter>", { noremap = true })
 
 
