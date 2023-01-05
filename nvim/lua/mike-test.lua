6752970ec596be19de14b3978142f5253a9f6a5f
Michael Iles <michael.iles@gmail.com>
2023-01-05 12:45:02 -0500

    Add initial version of function to toggle 'todo' checkboxes.

---
+++ nvim/lua/mike-test.lua
@@ -0,0 +1,43 @@
+lu = require('luaunit')
+require('mike')
+
+TestMike = {}
+
+TestMike.tests = {
+  -- Good
+  { "abc", "[ ] abc" },
+  { "[ ] abc", "[x] abc" },
+  { "[x] abc", "[ ] abc" },
+  { "+ abc", "+ [ ] abc" },
+  { "+ [ ] abc", "+ [x] abc" },
+  { "+ [x] abc", "+ [ ] abc" },
+  { "  + abc", "  + [ ] abc" },
+  { "  + [ ] abc", "  + [x] abc" },
+  { "  + [x] abc", "  + [ ] abc" },
+  { "  * abc", "  * [ ] abc" },
+  { "  * [ ] abc", "  * [x] abc" },
+  { "  * [x] abc", "  * [ ] abc" },
+  { "  - abc", "  - [ ] abc" },
+  { "  - [ ] abc", "  - [x] abc" },
+  { "  - [x] abc", "  - [ ] abc" },
+  { " [whatever]", " [ ] [whatever]" },
+
+  -- Malformed
+  { "[ abc", "[ ] [ abc" },
+  { "] abc", "[ ] ] abc" },
+  { "  [[] abc", "  [ ] [[] abc" },
+  { "[ ]abc", "[ ] [ ]abc" },
+}
+
+TestMike.listOfNameAndInst = {}
+
+for _, entry in pairs(TestMike.tests) do
+  local line = entry[1]
+  local expected = entry[2]
+  table.insert(TestMike.listOfNameAndInst, { line, function()
+      actual = mikemike(line)
+      lu.assertEquals(actual, expected)
+    end } )
+end
+
+lu.LuaUnit:runSuiteByInstances(TestMike.listOfNameAndInst, "")
---
+++ nvim/lua/mike.lua
@@ -0,0 +1,18 @@
+
+
+function mikemike (line)
+    spaces, list, check, body = string.match(line, "^(%s*)([+*-]? ?)([x [%]]* ?)(.*)")
+    if spaces ~= nil then
+        if check == "[x] " then
+            check = "[ ] "
+        elseif check  == "[ ] " then
+            check = "[x] "
+        else
+            check = "[ ] " .. check
+        end
+        return spaces .. list .. check .. body
+    end
+    return "1" .. line
+end
+
+
